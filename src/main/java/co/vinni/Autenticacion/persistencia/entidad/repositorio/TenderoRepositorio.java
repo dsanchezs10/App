bc51512abfa6c158569a258497305504cb8f537e
Dianassanchez <70119636+dsanchezs10@users.noreply.github.com>
2024-11-27 13:30:39 -0500

    Delete src directory

--- src/main/java/co/vinni/Autenticacion/persistencia/entidad/repositorio/TenderoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package co.vinni.Autenticacion.persistencia.entidad.repositorio;
-
-import co.vinni.Autenticacion.persistencia.entidad.Tendero;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TenderoRepositorio extends JpaRepository<Tendero, Long> {
-}
